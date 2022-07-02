4e5ceb59a1cf1a3892aba630c2271d1845ecc61d
sof1agarc1a <sofgar0614@gmail.com>
2022-07-02 16:30:03 +0200

    Fix styling

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -4,19 +4,35 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   width: 50%;
   height: 100vh;
-  border-left: 1px solid #d7dae0;
   padding: 40px;
+  border-left: 1px solid #d7dae0;
 `;
 
-const Title = styled.h2``;
+const Title = styled.h2`
+  font-weight: 600;
+`;
 
-const Description = styled.p``;
+const Description = styled.p`
+  margin-top: 16px;
+  line-height: 24px;
+  color: #4c4c4c;
+`;
+
+const Director = styled.p`
+  margin-top: 16px;
+  color: #4c4c4c;
+`;
 
 export const MovieInfo = () => {
   return (
     <Wrapper>
       <Title> Movie title </Title>
-      <Description> hejj jhdfjdsfjhds </Description>
+      <Description>
+        {" "}
+        hejj jhdfjdsfjhds jhdfjdsfjhds jhdfjdsfjhds jhdfjdsfjhds jhdfjdsfjhds
+        jhdfjdsfjhds jhdfjdsfjhds jhdfjdsfjhds{" "}
+      </Description>
+      <Director>Directed by: Invin </Director>
     </Wrapper>
   );
 };
--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -4,18 +4,34 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  border-bottom: 1px solid #d7dae0;
   padding: 20px 24px;
+  border-bottom: 1px solid #d7dae0;
+`;
+
+const EpisodeTitle = styled.span`
+  margin-left: 20px;
+  font-weight: 500;
+`;
+
+const EpisodeNumber = styled.span`
+  color: #4c4c4c;
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  letter-spacing: 0.05rem;
+`;
+
+const EpisodeDate = styled.span`
+  color: #4c4c4c;
 `;
 
 export const MovieItem = () => {
   return (
     <Wrapper>
       <div>
-        <span> Episode 4 </span>
-        <span> Episode IV - A New Hope </span>
+        <EpisodeNumber> Episode 4 </EpisodeNumber>
+        <EpisodeTitle> Episode IV - A New Hope </EpisodeTitle>
       </div>
-      <span> 1955-06-25 </span>
+      <EpisodeDate> 1955-06-25 </EpisodeDate>
     </Wrapper>
   );
 };
